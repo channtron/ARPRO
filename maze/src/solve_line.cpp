c53fcf8bf64a82a197ed6250618eb8c04a6fd296
Channtron <p.guti.jime@gmail.com>
2023-12-26 18:49:46 +0100

    initial commit for solve line

---
+++ maze/src/solve_line.cpp
@@ -0,0 +1,79 @@
+#include <a_star.h>
+#include <maze.h>
+
+using namespace std;
+using namespace ecn;
+
+// a node is a x-y position, we move from 1 each time
+class Position : public Point
+{
+public:
+    // constructor from coordinates
+    Position(int _x, int _y) : Point(_x, _y) {}
+
+    // constructor from base ecn::Point
+    Position(ecn::Point _p) : Point(_p.x, _p.y) {}
+
+    int distToParent()
+    {
+        // in cell-based motion, the distance to the parent is always 1
+        return 1;
+    }
+
+    int isCorridor(int incrementX, int incrementY)
+    {
+        int count = 1;
+        while(this->maze.isFree(this->x+incrementX*count, this->y+incrementY+count))
+        {
+            count++;
+        }
+        if (count>1) return count;
+        return -1;
+    }
+
+    std::vector<Position> children()
+    {
+        // this method should return  all positions reachable from this one
+        std::vector<Position> generated;
+
+        // TODO add free reachable positions from this point
+        //generated.reserve(4);
+        int dist = 0;
+        if (dist = isCorridor(1,0) > 0) generated.emplace_back(std::make_unique<Position>(this->x+dist, this->y));
+        if (dist = isCorridor(-1,0) > 0) generated.emplace_back(std::make_unique<Position>(this->x-dist, this->y));
+        if (dist = isCorridor(0,1) > 0) generated.emplace_back(std::make_unique<Position>(this->x, this->y+dist));
+        if (dist = isCorridor(0,-1) > 0) generated.emplace_back(std::make_unique<Position>(this->x, this->y-dist));
+
+
+
+
+
+        return generated;
+    }
+    //int m_distance;
+};
+
+
+
+int main( int argc, char **argv )
+{
+    // load file
+    std::string filename = Maze::mazeFile("maze.png");
+    if(argc == 2)
+        filename = std::string(argv[1]);
+
+    // let Point know about this maze
+    Position::maze.load(filename);
+
+    // initial and goal positions as Position's
+    Position start = Position::maze.start(),
+             goal = Position::maze.end();
+
+    // call A* algorithm
+    ecn::Astar(start, goal);
+
+    // save final image
+    Position::maze.saveSolution("cell");
+    cv::waitKey(0);
+
+}
